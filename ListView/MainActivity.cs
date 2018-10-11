2b25c7202b670755717ac323457d399e476bb54b
Patrick Teivas <teivas.2011@mail.ru>
2018-10-11 09:18:58 +0300

    COmmit

---
+++ ListView/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace ListView
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : ListActivity //AppCompatActivity
+    {
+        readonly string[] countries = new string[] { "eesti", "rootsi", "leedu", "soome", "syria" };
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            //SetContentView(Resource.Layout.activity_main);
+
+            ListAdapter = new CustomAdapter(this, countries);
+
+            ListView.ItemClick += ListView_ItemClick;
+        }
+
+        private void ListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            Toast.MakeText(Application, ((TextView)e.View).Text, ToastLength.Long).Show();
+        }
+    }
+}
